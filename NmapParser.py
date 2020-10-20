64a212527ecf374ecf0a092bbd98f191d9b3f9f8
vertering <jorik.heijstek@gmail.com>
2020-10-20 09:34:07 +0200

    Update NmapParser.py
    Latest version, dunno what I've changed since the last commit.

--- NmapParser.py
+++ NmapParser.py
@@ -1,44 +1,77 @@
-"""
-The Nmap XML output is unreadable and not usable by Excel. This script parses one or more XML files into an Excel
-sheet called NmapParser.xlsx
-Usage of the script is: python NmapParser.py nmap1.xml nmap2.xml
-"""
+"""The Nmap XML output is unreadable and not usable by Excel. This script parses one or more XML files into an Excel
+sheet. Note, in order to show hosts that are down, nmap should run with the -v flag In order to run this xlswriter
+must be installed: pip install xlsxwriter. Usage of
+the script is: python NmapParser.py nmap1.xml nmap2.xml """
 
 from xml.etree import ElementTree as Et
 import sys
 import xlsxwriter
 
+headerResult = ["IP", "Hostname", "Protocol", "Port", "Open", "Version"]
 resultList = []
-header = ["IP", "Hostname", "Protocol", "Port", "Open", "Version"]
-resultList.append(header)
+resultList.append(headerResult)
+headerHosts = ["IP", "Hostname"]
+upHosts = []
+upHosts.append(headerHosts)
+downHosts = []
+downHosts.append(headerHosts)
 for argument in sys.argv[1:]:
     tree = Et.parse(argument)
     root = tree.getroot()
     for host in root.iter("host"):
-        up = host.find("status").attrib
-        if (up["state"]) == "up":
+        status = host.find("status").attrib
+        if (status["state"]) == "up":
             address = host.find("address").attrib["addr"]
             hostname = None
-            for hostnames in host.find("hostnames"):
-                if "name" in hostnames.attrib:
-                    hostname = hostnames.attrib["name"]
+            protocol = None
+            portId = None
+            state = None
+            version = None
+            for hostNames in host.find("hostnames"):
+                if "name" in hostNames.attrib:
+                    hostname = hostNames.attrib["name"]
+            upHost = (address, hostname)
+            upHosts.append(upHost)
             for ports in host.findall("ports"):
-                for port in ports.findall("port"):
-                    protocol = port.attrib["protocol"]
-                    portid = port.attrib["portid"]
-                    state = None
-                    for filter in port.findall("state"):
-                        state = filter.attrib["state"]
-                    version = None
-                    for name in port.findall("service"):
-                        if "product" in name.attrib:
-                            version = name.attrib["product"]
-                    result = (address, hostname, protocol, portid, state, version)
+                # For host element and port sub element without any ports
+                if not ports.findall("port"):
+                    result = (address, hostname, protocol, portId, state, version)
                     resultList.append(result)
+                # For host element and port sub element with ports
+                else:
+                    for port in ports.findall("port"):
+                        protocol = port.attrib["protocol"]
+                        portId = port.attrib["portid"]
+                        for filtered in port.findall("state"):
+                            state = filtered.attrib["state"]
+                        for name in port.findall("service"):
+                            if "product" in name.attrib:
+                                version = name.attrib["product"]
+                        result = (address, hostname, protocol, portId, state, version)
+                        resultList.append(result)
+        if (status["state"]) == "down":
+            address = host.find("address").attrib["addr"]
+            hostname = None
+            if host.find("hostnames") is not None:
+                for hostNames in host.find("hostnames"):
+                    if "name" in hostNames.attrib:
+                        hostname = hostNames.attrib["name"]
+            downHost = (address, hostname)
+            downHosts.append(downHost)
 workbook = xlsxwriter.Workbook('NmapParser.xlsx')
-worksheet = workbook.add_worksheet()
+worksheet = workbook.add_worksheet("Results")
 rowNumber = 3
 for result in resultList:
+    worksheet.write_row("C" + str(rowNumber), result)
+    rowNumber += 1
+worksheet = workbook.add_worksheet("Hosts up")
+rowNumber = 3
+for result in upHosts:
+    worksheet.write_row("C" + str(rowNumber), result)
+    rowNumber += 1
+worksheet = workbook.add_worksheet("Hosts down")
+rowNumber = 3
+for result in downHosts:
     worksheet.write_row("C" + str(rowNumber), result)
     rowNumber += 1
 workbook.close()

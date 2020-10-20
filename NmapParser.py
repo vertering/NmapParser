c5e4ff3be0d947334ddf36574dd6cda1d22c6b37
vertering <jorik.heijstek@gmail.com>
2020-10-20 09:35:38 +0200

    Update NmapParser.py

--- NmapParser.py
+++ NmapParser.py
@@ -1,7 +1,6 @@
 """The Nmap XML output is unreadable and not usable by Excel. This script parses one or more XML files into an Excel
 sheet. Note, in order to show hosts that are down, nmap should run with the -v flag In order to run this xlswriter
-must be installed: pip install xlsxwriter. Usage of
-the script is: python NmapParser.py nmap1.xml nmap2.xml """
+must be installed: pip install xlsxwriter. Usage of the script is: python NmapParser.py nmap1.xml nmap2.xml """
 
 from xml.etree import ElementTree as Et
 import sys
